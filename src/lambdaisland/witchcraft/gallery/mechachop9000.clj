e5e01d530c3bb6e076f55ed91bd812cb7d3cef39
Arne Brasseur <arne@arnebrasseur.net>
2021-12-03 12:19:16 +0100

    Add the MegaChop9000 to the gallery

---
+++ src/lambdaisland/witchcraft/gallery/mechachop9000.clj
@@ -0,0 +1,36 @@
+(ns lambdaisland.witchcraft.gallery.mechachop9000
+  "A magical dwarven axe which chops down a full tree trunk with one chop."
+  (:require [lambdaisland.witchcraft :as wc]
+            [lambdaisland.witchcraft.events :as e]
+            [lambdaisland.witchcraft.markup :as markup]))
+
+(def megachop9000-name
+  (wc/normalize-text
+   (markup/fAnCy "MegaChop 9000"
+                 [:yellow :gold :red])))
+
+(defn create-megachop []
+  (let [axe (wc/item-stack :diamond-axe)]
+    (wc/set-display-name axe megachop9000-name)
+    (wc/set-lore axe
+                 [[:italic "Even the trees shivered"]
+                  [:italic
+                   "when the "
+                   [:bold "Themjer"] " passed."]])
+    axe))
+
+(defn give-to-player [player]
+  (wc/add-inventory player (create-megachop)))
+
+(defn register-listener! []
+  (e/listen!
+   :player-interact
+   ::megachop-9000
+   (fn [{:keys [clickedBlock player action]}]
+     (when clickedBlock
+       (let [material (wc/material-name clickedBlock)]
+         (when (and (= megachop9000-name (wc/display-name (wc/item-in-hand player)))
+                    (re-find #"-log$" (name material)))
+           (run! (memfn breakNaturally)
+                 (fill clickedBlock {:pred #(= material (wc/material-name %))
+                                     :dy [-1 0 1]}))))))))
