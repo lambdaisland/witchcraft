c0e627fa7ec95fe53e65cff3a55a77fb2eda60e6
Arne Brasseur <arne@arnebrasseur.net>
2022-01-11 08:22:59 -0500

    Add example of a trait implementation

---
+++ src/lambdaisland/witchcraft/gallery/carrier_trait.clj
@@ -0,0 +1,28 @@
+(ns lambdaisland.witchcraft.gallery.carrier-trait
+  "A trait for NPCs that are able to carry stuff around for you. Right click to
+  open their inventory and put stuff in it."
+  (:require [lambdaisland.witchcraft :as wc]
+            [lambdaisland.witchcraft.citizens :as c]
+            [lambdaisland.witchcraft.events :as e]))
+
+(c/make-trait
+ :carrier
+ {:init {:inventory (wc/make-inventory {:capacity 18})}})
+
+(e/listen!
+ :citizens/npcright-click
+ ::carrier
+ (fn [e]
+   (when (c/has-trait? (:NPC e) :carrier)
+     (wc/open-inventory
+      (:clicker e)
+      (:inventory @(c/npc-trait (:NPC e) :carrier))))))
+
+(comment
+  (def jonny (create-npc :player "jonny"))
+  ;; or
+  (def jonny (c/npc-by-id 0))
+
+  (c/update-traits jonny {:carrier {}})
+
+  )
